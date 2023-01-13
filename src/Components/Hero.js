24f6194ba7c3f2cdced8ad37d4ed24d2b604e6d0
Minerva Dutta <dutta.minerva@gmail.com>
2023-01-13 14:08:48 -0800

    first commit

---
+++ src/Components/Hero.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Hero.css'
+import logoDark from '../Images/logoDark.png'  
+
+function Hero() {
+  return (
+    <>
+    <header className="hero-section">
+        <div className="content" >
+            <img src={logoDark} className="logo" alt=""/>
+            {/*  */}
+            <p className="sub-heading">4Geeks House of Fashion</p>
+        </div>
+    </header>
+    </>
+  )
+}
+
+export default Hero
+
