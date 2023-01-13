8a02d980646e9a40dc57448ba4de0aca7e54044f
Minerva Dutta <dutta.minerva@gmail.com>
2023-01-13 15:28:45 -0800

    completed footer with bootstrap

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,55 +1,86 @@
 import React from 'react'
-import logoLight from '../Images/logoLight.png' 
+import alex from '../Images/alex.png' 
+import ashley from '../Images/ashley.png'
+import minerva from '../Images/minerva.png'  
 import './Footer.css'
 
 function Footer() {
   return (
     <>
-    <div class="footer-content">
-            <img src={logoLight} class="logo" alt=""/>
-            <div class="footer-ul-container">
-                <ul class ="category">
-                    <li class="category-title">men</li>
-                    <li><a href="#" class="footer-links">t-shirts</a></li>
-                    <li><a href="#" class="footer-links">sweatshirts</a></li>
-                    <li><a href="#" class="footer-links">shirts</a></li>
-                    <li><a href="#" class="footer-links">jeans</a></li>
-                    <li><a href="#" class="footer-links">shoes</a></li>
-                    <li><a href="#" class="footer-links">casuals</a></li>
-                    <li><a href="#" class="footer-links">formals</a></li>
-                    <li><a href="#" class="footer-links">sports</a></li>
-                    <li><a href="#" class="footer-links">watch</a></li>
-                </ul>
-                <ul class ="category">
-                    <li class="category-title">women</li>
-                    <li><a href="#" class="footer-links">t-shirts</a></li>
-                    <li><a href="#" class="footer-links">sweatshirts</a></li>
-                    <li><a href="#" class="footer-links">shirts</a></li>
-                    <li><a href="#" class="footer-links">jeans</a></li>
-                    <li><a href="#" class="footer-links">shoes</a></li>
-                    <li><a href="#" class="footer-links">casuals</a></li>
-                    <li><a href="#" class="footer-links">formals</a></li>
-                    <li><a href="#" class="footer-links">sports</a></li>
-                    <li><a href="#" class="footer-links">watch</a></li>
-                </ul>
-            </div>
+    <footer>
+  <div className="container">
+    <div className="row">
+      
+      <div className="col-lg-4 col-md-6">
+        <h3>Site Map</h3>
+        <ul className="list-unstyled three-column">
+          <li>Home</li>
+          <li>Services</li>
+          <li>About</li>
+          <li>Code</li>
+          <li>Design</li>
+          <li>Host</li>
+          <li>Contact</li>
+          <li>Company</li>
+        </ul>
+        <ul className="list-unstyled socila-list">
+          <li><img src="http://placehold.it/48x48" alt="" /></li>
+          <li><img src="http://placehold.it/48x48" alt="" /></li>
+          <li><img src="http://placehold.it/48x48" alt="" /></li>
+          <li><img src="http://placehold.it/48x48" alt="" /></li>
+          <li><img src="http://placehold.it/48x48" alt="" /></li>
+          <li><img src="http://placehold.it/48x48" alt="" /></li>
+        </ul>
+      </div>
+      
+      <div className="col-lg-4 col-md-6">
+        <h3>latest Articles</h3>
+        <div className="media">
+          <a href="#" className="pull-left">
+            <img src="http://placehold.it/64x64" alt="" className="media-object" />
+          </a>
+          <div className="media-body">
+            <h4 className="media-heading">Programming</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+          </div>
         </div>
-        <p class="footer-title">about company</p>
-        <p class="info">Know more about our mission and vision</p>
-        <p class="info">support emails - help@aandm.com</p>
-        <p class="info">telephone - 1800 555 555 </p>
-        <div class="footer-social-container">
-            <div>
-                <a href="#" class="social-link">terms & services</a>
-                <a href="#" class="social-link">privacy page</a>
-            </div>
-            <div>
-                <a href="#" class="social-link">instagram</a>
-                <a href="#" class="social-link">facebook</a>
-                <a href="#" class="social-link">twitter</a>
-            </div>
+        
+        <div className="media">
+          <a href="#" className="pull-left">
+            <img src="http://placehold.it/64x64" alt="" className="media-object" />
+          </a>
+          <div className="media-body">
+            <h4 className="media-heading">Coding</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+          </div>
         </div>
-        <p class="footer-credit">The most enviromentally conscious online retail store</p>
+        
+        <div className="media">
+          <a href="#" className="pull-left">
+            <img src="http://placehold.it/64x64" alt="" className="media-object" />
+          </a>
+          <div className="media-body">
+            <h4 className="media-heading">Web Sesign</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+          </div>
+        </div>
+        
+      </div>
+      
+      <div className="col-lg-4">
+        <h3>Our Work</h3>
+        <img className="img-thumbnail" src={alex} alt="" />
+        <img className="img-thumbnail" src={ashley} alt="" />
+        <img className="img-thumbnail" src={minerva} alt="" />
+        
+      </div>
+      
+    </div>
+  </div>
+  <div className="copyright text-center">
+    Copyright &copy; 2017 <span>Your Template Name</span>
+  </div>
+</footer>
     </>
   )
 }
