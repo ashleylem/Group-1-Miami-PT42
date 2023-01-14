02d830aed6f2731111a4ac08ed7e68d5c75904b6
Minerva Dutta <dutta.minerva@gmail.com>
2023-01-13 23:51:46 -0800

    added src

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import Hero from './Components/Hero';
+import Nav from './Components/Nav';
+import Footer from './Components/Footer';
+import Bestsellers from './Components/Bestsellers';
+import Shirts from './Components/Shirts';
+import Shoes from './Components/Shoes';
+import Collections from './Components/Collections';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Nav />
+    <Hero />
+    <Bestsellers />
+    <Collections/>
+    <Shirts />
+    <Shoes />
+    <Footer />
+    </>
   );
+  
 }
 
 export default App;
