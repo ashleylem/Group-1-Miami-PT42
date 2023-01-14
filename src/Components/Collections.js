a9c061b83b1f2c2634124d0882d1b766f6530e1b
Minerva Dutta <dutta.minerva@gmail.com>
2023-01-13 23:28:36 -0800

    added collections

---
+++ src/Components/Collections.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import './Hero.css'
+import womenCollection from '../Images/women-collection.png'
+import menCollection from '../Images/men-collection.png'
+import accessoriesCollection from '../Images/accessories-collection.png'
+
+function Collections() {
+  return (
+    <>
+     <section class="collection-container">
+        <a href="#" class="collection" onclick="trackClick3()">
+            <img src={womenCollection} alt=""/>
+            <p class="collection-title">for<br/>women</p>
+
+            
+        </a>
+
+        <a href="#" class="collection" onclick="trackClick2()">
+            <img src={menCollection} alt=""/>
+            <p class="collection-title">for<br/>men</p>
+
+            
+        </a>
+
+        <a href="#" class="collection" onclick="trackClick1()">
+            <img src={accessoriesCollection} alt=""/>
+            <p class="collection-title">accessories</p>
+
+            
+        </a>
+    </section>
+    </>
+  )
+}
+
+export default Collections
