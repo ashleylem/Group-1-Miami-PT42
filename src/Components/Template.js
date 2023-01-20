10bd11f563b6db1743db25db5e867507b6ac6a44
minervaDutta <dutta.minerva@gmail.com>
2023-01-20 03:01:42 +0000

    added Footer in its own folder

--- src/Components/Template.js
+++ src/Components/Template.js
@@ -1,6 +1,6 @@
 import Hero from './Hero';
 import Nav from './Nav';
-import Footer from './Footer';
+import Footer from './Footer/Footer';
 import {Outlet} from 'react-router-dom';
 import React from 'react'
 
